f14c592008fcbaa8f7d6e1000f6048480b9440f5
Komang Gede Narariya Suputra <91647402+16Komang@users.noreply.github.com>
2022-12-12 16:12:41 +0700

    flutter_sqflite

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqflite/page/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tambahkan Item',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: Home(),
+    );
+  }
+}
